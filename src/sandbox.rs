abb4867abc86c99d723677d8fb43c23be9a049ec
MeiK <meik2333@gmail.com>
2023-07-11 20:29:29 +0800

    修改报错格式

--- src/sandbox.rs
+++ src/sandbox.rs
@@ -1,28 +1,25 @@
 use crate::error::Result;
-use log::{debug, trace};
 use std::ffi::c_void;
 use std::mem::size_of;
 
 use crate::error::Error::WinError;
 use crate::status::Status;
 
-use windows::Win32::Foundation::{BOOL, FILETIME, WAIT_FAILED, WAIT_TIMEOUT};
-use windows::Win32::System::JobObjects::{
-    AssignProcessToJobObject, CreateJobObjectA, JobObjectBasicLimitInformation,
-    SetInformationJobObject, JOBOBJECT_BASIC_LIMIT_INFORMATION, JOB_OBJECT_LIMIT_ACTIVE_PROCESS,
-    JOB_OBJECT_LIMIT_PRIORITY_CLASS, JOB_OBJECT_LIMIT_PROCESS_TIME, JOB_OBJECT_LIMIT_WORKINGSET,
-};
-use windows::Win32::System::ProcessStatus::PROCESS_MEMORY_COUNTERS;
-use windows::Win32::System::Threading::{
-    GetProcessTimes, SetProcessWorkingSetSize, TerminateProcess, IDLE_PRIORITY_CLASS,
-};
 use windows::{
     core::{PCSTR, PSTR},
     Win32::Foundation::GetLastError,
+    Win32::Foundation::{FILETIME, WAIT_FAILED, WAIT_TIMEOUT},
+    Win32::System::JobObjects::{
+        AssignProcessToJobObject, CreateJobObjectA, JobObjectBasicLimitInformation,
+        SetInformationJobObject, JOBOBJECT_BASIC_LIMIT_INFORMATION,
+        JOB_OBJECT_LIMIT_PRIORITY_CLASS, JOB_OBJECT_LIMIT_PROCESS_TIME,
+    },
     Win32::System::ProcessStatus::GetProcessMemoryInfo,
-    Win32::System::Threading,
+    Win32::System::ProcessStatus::PROCESS_MEMORY_COUNTERS,
     Win32::System::Threading::{
-        WaitForSingleObject, CREATE_SUSPENDED, PROCESS_INFORMATION, STARTUPINFOA,
+        CreateProcessA, GetProcessTimes, ResumeThread, SetProcessWorkingSetSize, TerminateProcess,
+        WaitForSingleObject, CREATE_SUSPENDED, IDLE_PRIORITY_CLASS, PROCESS_INFORMATION,
+        STARTUPINFOA,
     },
 };
 
@@ -33,7 +30,6 @@ pub struct Sandbox {
     pub memory_limit: Option<u32>,
 }
 
-#[cfg(target_os = "windows")]
 impl Sandbox {
     pub fn new(args: Vec<String>) -> Self {
         Sandbox {
@@ -56,9 +52,22 @@ impl Sandbox {
         self.memory_limit = l;
         self
     }
+}
 
-    pub fn run(&mut self) -> Result<Status> {
-        // TODO: 函数功能拆分和 unsafe 作用域整改
+#[macro_export]
+macro_rules! winapi_bool {
+    ($expression:expr) => {
+        if $expression.as_bool() == false {
+            return Err(WinError(String::from(file!()), line!(), unsafe {
+                GetLastError()
+            }));
+        }
+    };
+}
+
+#[cfg(target_os = "windows")]
+impl Sandbox {
+    pub unsafe fn run(&mut self) -> Result<Status> {
         let mut status: Status = Default::default();
         // 执行的目标 app，前置的命令行解析保证 inner_args 至少有一项
         let app = Vec::from((&self.inner_args[0]).as_bytes()).as_ptr();
@@ -73,84 +82,27 @@ impl Sandbox {
         let mut info: STARTUPINFOA = Default::default();
         let mut information: PROCESS_INFORMATION = Default::default();
 
-        let code = unsafe {
-            Threading::CreateProcessA(
-                PCSTR::from_raw(app),
-                command_line_pstr,
-                None,
-                None,
-                false,
-                // CREATE_SUSPENDED: 创建一个暂停的进程，需要 ResumeThread 之后才可以正常运行
-                CREATE_SUSPENDED,
-                None,
-                None,
-                &mut info,
-                &mut information,
-            )
-        };
-        if code.ok().is_err() {
-            return Err(WinError(String::from("CreateProcessA"), unsafe {
+        winapi_bool!(CreateProcessA(
+            PCSTR::from_raw(app),
+            command_line_pstr,
+            None,
+            None,
+            false,
+            // CREATE_SUSPENDED: 创建一个暂停的进程，需要 ResumeThread 之后才可以正常运行
+            CREATE_SUSPENDED,
+            None,
+            None,
+            &mut info,
+            &mut information,
+        ));
+
+        self.limit(&information)?;
+
+        // 唤醒被暂停的进程
+        if ResumeThread(information.hThread) != 1 {
+            return Err(WinError(String::from(file!()), line!(), unsafe {
                 GetLastError()
             }));
-        }
-
-        let job = unsafe { CreateJobObjectA(None, None) };
-        if job.is_err() {
-            return Err(WinError(String::from("CreateJobObjectA"), unsafe {
-                GetLastError()
-            }));
-        }
-        let job = job.ok().unwrap();
-
-        let mut limit: JOBOBJECT_BASIC_LIMIT_INFORMATION = Default::default();
-        limit.LimitFlags = JOB_OBJECT_LIMIT_PRIORITY_CLASS;
-        limit.PriorityClass = IDLE_PRIORITY_CLASS.0;
-
-        // 系统定期检查以确定与作业关联的每个进程是否累积了比设置限制更多的用户模式时间。 如果已终止，则终止进程。
-        // cpu 时间限制，此限制不会实时结束进程（需要等到下次检查？）
-        if let Some(l) = self.cpu_time_limit {
-            limit.LimitFlags |= JOB_OBJECT_LIMIT_PROCESS_TIME;
-            limit.PerProcessUserTimeLimit = (l as i64 * 10000);
-            limit.PerJobUserTimeLimit = (l as i64 * 10000);
-        }
-
-        // 内存限制，与 cpu 时间限制类似，此限制并不能保证可用性
-        if let Some(l) = self.memory_limit {
-            unsafe {
-                SetProcessWorkingSetSize(information.hProcess, 1, l as usize * 1024);
-            }
-        }
-
-        unsafe {
-            // 设置 job 限制
-            let res = SetInformationJobObject(
-                job,
-                JobObjectBasicLimitInformation,
-                &limit as *const _ as *const c_void,
-                size_of::<JOBOBJECT_BASIC_LIMIT_INFORMATION>() as u32,
-            );
-            if res == BOOL(0) {
-                return Err(WinError(String::from("SetInformationJobObject"), unsafe {
-                    GetLastError()
-                }));
-            }
-        }
-
-        unsafe {
-            // 将 job 附加到进程
-            let res = AssignProcessToJobObject(job, information.hProcess);
-            if res == BOOL(0) {
-                return Err(WinError(String::from("AssignProcessToJobObject"), unsafe {
-                    GetLastError()
-                }));
-            }
-        }
-
-        unsafe {
-            // 唤醒被暂停的进程
-            if Threading::ResumeThread(information.hThread) != 1 {
-                return Err(WinError(String::from("ResumeThread"), GetLastError()));
-            }
         }
 
         let timeout = if let Some(t) = self.time_limit {
@@ -160,54 +112,92 @@ impl Sandbox {
             0xFFFFFFFF
         };
 
-        let wait_ret = unsafe { WaitForSingleObject(information.hProcess, timeout) };
+        let wait_ret = WaitForSingleObject(information.hProcess, timeout);
         if wait_ret == WAIT_TIMEOUT {
             // 超时中断进程
-            unsafe {
-                debug!("Terminated due to timeout");
-                if TerminateProcess(information.hProcess, 0).ok().is_err() {
-                    return Err(WinError(String::from("TerminateProcess"), GetLastError()));
-                }
-                WaitForSingleObject(information.hProcess, 0xFFFFFFFF);
-            }
+            winapi_bool!(TerminateProcess(information.hProcess, 0));
+            // 此处不检查返回值
+            WaitForSingleObject(information.hProcess, 0xFFFFFFFF);
         } else if wait_ret == WAIT_FAILED {
-            return Err(WinError(String::from("WaitForSingleObject"), unsafe {
+            return Err(WinError(String::from(file!()), line!(), unsafe {
                 GetLastError()
             }));
         }
 
         let mut pmc: PROCESS_MEMORY_COUNTERS = Default::default();
 
-        unsafe {
-            // 获取内存使用情况
-            GetProcessMemoryInfo(
-                information.hProcess,
-                &mut pmc,
-                size_of::<PROCESS_MEMORY_COUNTERS>() as u32,
-            );
-            status.memory_used = (pmc.PeakWorkingSetSize / 1024) as u64;
-        }
+        // 获取内存使用情况
+        winapi_bool!(GetProcessMemoryInfo(
+            information.hProcess,
+            &mut pmc,
+            size_of::<PROCESS_MEMORY_COUNTERS>() as u32,
+        ));
+
+        status.memory_used = (pmc.PeakWorkingSetSize / 1024) as u64;
 
         let mut lp_creation_time: FILETIME = Default::default();
         let mut lp_exit_time: FILETIME = Default::default();
         let mut lp_kernel_time: FILETIME = Default::default();
         let mut lp_user_time: FILETIME = Default::default();
-        unsafe {
-            GetProcessTimes(
-                information.hProcess,
-                &mut lp_creation_time,
-                &mut lp_exit_time,
-                &mut lp_kernel_time,
-                &mut lp_user_time,
-            );
-        }
+        winapi_bool!(GetProcessTimes(
+            information.hProcess,
+            &mut lp_creation_time,
+            &mut lp_exit_time,
+            &mut lp_kernel_time,
+            &mut lp_user_time,
+        ));
 
-        // TODO: 需要判断一下，防止溢出和除零
         status.time_used =
             (lp_exit_time.dwLowDateTime - lp_creation_time.dwLowDateTime) as u64 / 10000;
         status.cpu_time_used =
             (lp_kernel_time.dwLowDateTime + lp_user_time.dwLowDateTime) as u64 / 10000;
 
         Ok(status)
+    }
+
+    unsafe fn limit(&mut self, information: &PROCESS_INFORMATION) -> Result<Status> {
+        let job = match CreateJobObjectA(None, None) {
+            Ok(resp) => resp,
+            Err(_) => {
+                return Err(WinError(String::from(file!()), line!(), unsafe {
+                    GetLastError()
+                }));
+            }
+        };
+
+        let mut limit: JOBOBJECT_BASIC_LIMIT_INFORMATION = Default::default();
+        limit.LimitFlags = JOB_OBJECT_LIMIT_PRIORITY_CLASS;
+        limit.PriorityClass = IDLE_PRIORITY_CLASS.0;
+
+        // 系统定期检查以确定与作业关联的每个进程是否累积了比设置限制更多的用户模式时间。 如果已终止，则终止进程。
+        // cpu 时间限制，此限制不会实时结束进程（需要等到下次检查？）
+        if let Some(l) = self.cpu_time_limit {
+            limit.LimitFlags |= JOB_OBJECT_LIMIT_PROCESS_TIME;
+            limit.PerProcessUserTimeLimit = l as i64 * 10000;
+            limit.PerJobUserTimeLimit = l as i64 * 10000;
+        }
+
+        // 内存限制
+        if let Some(l) = self.memory_limit {
+            // 与 cpu 时间限制类似，此限制并不能保证可用性
+            winapi_bool!(SetProcessWorkingSetSize(
+                information.hProcess,
+                1,
+                l as usize * 1024
+            ))
+        }
+
+        // 设置 job 限制
+        winapi_bool!(SetInformationJobObject(
+            job,
+            JobObjectBasicLimitInformation,
+            &limit as *const _ as *const c_void,
+            size_of::<JOBOBJECT_BASIC_LIMIT_INFORMATION>() as u32,
+        ));
+
+        // 将 job 附加到进程
+        winapi_bool!(AssignProcessToJobObject(job, information.hProcess));
+
+        Ok(Default::default())
     }
 }
