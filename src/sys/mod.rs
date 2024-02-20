00e2bb0a42b712e4d6ba99083f0605e66d4268ec
MeiK <meik2333@gmail.com>
2024-02-20 19:44:16 +0800

    Update

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -1,14 +1,14 @@
 #[cfg(target_os = "macos")]
-pub mod macos;
-#[cfg(target_os = "macos")]
-pub use crate::sys::macos::*;
-
+pub(crate) mod macos;
 #[cfg(target_os = "linux")]
-pub mod linux;
-#[cfg(target_os = "linux")]
-pub use crate::sys::linux::*;
+pub(crate) mod linux;
+#[cfg(target_os = "windows")]
+pub(crate) mod windows;
 
-#[cfg(target_os = "windows")]
-pub mod windows;
-#[cfg(target_os = "windows")]
-pub use crate::sys::windows::*;
+
+pub trait Sys {
+    /**
+     * run
+     */
+    fn run() -> ();
+}
