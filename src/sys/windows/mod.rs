1d9982226dc3ef64b089f96f72a56b6c786315c8
MeiK <meik2333@gmail.com>
2024-02-28 15:18:02 +0800

    Windows 平台支持基础命令调用

--- src/sys/windows/mod.rs
+++ src/sys/windows/mod.rs
@@ -1,20 +1,33 @@
 use std::ffi::c_void;
 use std::mem::size_of;
+
 use windows::core::{PCSTR, PSTR};
-use windows::Win32::Foundation::{GetLastError, WAIT_FAILED, WAIT_TIMEOUT};
+use windows::Win32::Foundation::{FILETIME, WAIT_FAILED, WAIT_TIMEOUT};
 use windows::Win32::System::JobObjects::{
-    AssignProcessToJobObject, CreateJobObjectA, JobObjectBasicLimitInformation,
-    SetInformationJobObject, JOBOBJECT_BASIC_LIMIT_INFORMATION, JOB_OBJECT_LIMIT_PRIORITY_CLASS,
-    JOB_OBJECT_LIMIT_PROCESS_TIME,
+    AssignProcessToJobObject, CreateJobObjectA, JOB_OBJECT_LIMIT_PRIORITY_CLASS,
+    JOB_OBJECT_LIMIT_PROCESS_TIME, JOBOBJECT_BASIC_LIMIT_INFORMATION, JobObjectBasicLimitInformation,
+    SetInformationJobObject,
 };
+use windows::Win32::System::ProcessStatus::{GetProcessMemoryInfo, PROCESS_MEMORY_COUNTERS};
 use windows::Win32::System::Threading::{
-    CreateProcessA, ResumeThread, SetProcessWorkingSetSize, TerminateProcess, WaitForSingleObject,
-    CREATE_SUSPENDED, IDLE_PRIORITY_CLASS, PROCESS_INFORMATION, STARTUPINFOA,
+    CREATE_SUSPENDED, CreateProcessA, GetProcessTimes, IDLE_PRIORITY_CLASS, PROCESS_INFORMATION,
+    ResumeThread, SetProcessWorkingSetSize, STARTUPINFOA, TerminateProcess, WaitForSingleObject,
 };
 
-use crate::error::Error::{WinError, E};
-use crate::sys::SandboxImpl;
+use crate::error::Error::{E, WinError};
+use crate::error::Result;
 use crate::Opts;
+use crate::status::Status;
+use crate::sys::SandboxImpl;
+
+#[macro_export]
+macro_rules! winapi {
+    ($expression:expr) => {
+        if let Err(e) = $expression {
+            return Err(WinError(String::from(file!()), line!(), e));
+        }
+    };
+}
 
 #[derive(Debug)]
 pub struct Sandbox {
@@ -29,7 +42,7 @@ pub struct Sandbox {
 }
 
 impl Sandbox {
-    unsafe fn set_limit(&mut self, information: &PROCESS_INFORMATION) -> crate::error::Result<()> {
+    unsafe fn set_limit(&mut self, information: &PROCESS_INFORMATION) -> Result<()> {
         // 创建 JOB
         let job = match CreateJobObjectA(None, None) {
             Ok(j) => j,
@@ -43,9 +56,11 @@ impl Sandbox {
         // 内存限制
         if let Some(l) = self.memory_limit {
             // 与 cpu 时间限制类似，此限制并不能保证可用性
-            if let Err(e) = SetProcessWorkingSetSize(information.hProcess, 1, l as usize * 1024) {
-                return Err(WinError(file!().to_string(), line!(), e));
-            }
+            winapi!(SetProcessWorkingSetSize(
+                information.hProcess,
+                1,
+                l as usize * 1024
+            ));
         }
 
         // 系统定期检查以确定与作业关联的每个进程是否累积了比设置限制更多的用户模式时间。 如果已终止，则终止进程。
@@ -57,22 +72,19 @@ impl Sandbox {
         }
 
         // 设置 job 限制
-        if let Err(e) = SetInformationJobObject(
+        winapi!(SetInformationJobObject(
             job,
             JobObjectBasicLimitInformation,
             &limit as *const _ as *const c_void,
             size_of::<JOBOBJECT_BASIC_LIMIT_INFORMATION>() as u32,
-        ) {
-            return Err(WinError(file!().to_string(), line!(), e));
-        }
+        ));
         // 将 job 附加到进程
-        if let Err(e) = AssignProcessToJobObject(job, information.hProcess) {
-            return Err(WinError(file!().to_string(), line!(), e));
-        }
+        winapi!(AssignProcessToJobObject(job, information.hProcess));
         Ok(())
     }
 
-    unsafe fn wait_it(&mut self, information: &PROCESS_INFORMATION) -> crate::error::Result<()> {
+    unsafe fn wait_it(&mut self, information: &PROCESS_INFORMATION) -> Result<Status> {
+        let mut status: Status = Default::default();
         let timeout = if let Some(t) = self.time_limit {
             t
         } else {
@@ -82,15 +94,43 @@ impl Sandbox {
         let wait_ret = WaitForSingleObject(information.hProcess, timeout);
         if wait_ret == WAIT_TIMEOUT {
             // 超时中断进程
-            if let Err(e) = TerminateProcess(information.hProcess, 0) {
-                return Err(WinError(file!().to_string(), line!(), e));
-            }
+            winapi!(TerminateProcess(information.hProcess, 0));
             // 此处不检查返回值
             WaitForSingleObject(information.hProcess, 0xFFFFFFFF);
         } else if wait_ret == WAIT_FAILED {
             return Err(E(file!().to_string(), line!(), "WAIT_FAILED".to_string()));
         }
-        Ok(())
+
+        let mut pmc: PROCESS_MEMORY_COUNTERS = Default::default();
+
+        // 获取内存使用情况
+        winapi!(GetProcessMemoryInfo(
+            information.hProcess,
+            &mut pmc,
+            size_of::<PROCESS_MEMORY_COUNTERS>() as u32,
+        ));
+
+        status.memory_used = (pmc.PeakWorkingSetSize / 1024) as u64;
+
+        // 获取时间使用情况
+        let mut lp_creation_time: FILETIME = Default::default();
+        let mut lp_exit_time: FILETIME = Default::default();
+        let mut lp_kernel_time: FILETIME = Default::default();
+        let mut lp_user_time: FILETIME = Default::default();
+        winapi!(GetProcessTimes(
+            information.hProcess,
+            &mut lp_creation_time,
+            &mut lp_exit_time,
+            &mut lp_kernel_time,
+            &mut lp_user_time,
+        ));
+
+        status.time_used =
+            (lp_exit_time.dwLowDateTime - lp_creation_time.dwLowDateTime) as u64 / 10000;
+        status.cpu_time_used =
+            (lp_kernel_time.dwLowDateTime + lp_user_time.dwLowDateTime) as u64 / 10000;
+
+        Ok(status)
     }
 }
 
@@ -108,7 +148,7 @@ impl SandboxImpl for Sandbox {
         }
     }
 
-    unsafe fn run(&mut self) -> crate::error::Result<()> {
+    unsafe fn run(&mut self) -> Result<Status> {
         // 执行的目标 app，前置的命令行解析保证 inner_args 至少有一项
         let app = Vec::from((&self.inner_args[0]).as_bytes()).as_ptr();
         // 执行的文件参数
@@ -123,7 +163,7 @@ impl SandboxImpl for Sandbox {
         let mut information: PROCESS_INFORMATION = Default::default();
 
         // 创建进程
-        if let Err(e) = CreateProcessA(
+        winapi!(CreateProcessA(
             PCSTR::from_raw(app),
             command_line_pstr,
             None,
@@ -135,18 +175,18 @@ impl SandboxImpl for Sandbox {
             None,
             &mut info,
             &mut information,
-        ) {
-            return Err(WinError(String::from(file!()), line!(), e));
-        }
+        ));
 
         self.set_limit(&information)?;
 
+        let resume = ResumeThread(information.hThread);
+
         // 唤醒被暂停的进程
-        if ResumeThread(information.hThread) != 1 {
+        if resume != 1 {
             return Err(E(
                 String::from(file!()),
                 line!(),
-                "唤醒进程失败".to_string(),
+                format!("唤醒进程失败，resume = {}", resume),
             ));
         }
 
@@ -156,10 +196,9 @@ impl SandboxImpl for Sandbox {
 
 #[cfg(test)]
 mod tests {
-    use crate::error::Result;
-    use crate::sys::windows::Sandbox;
-    use crate::sys::SandboxImpl;
     use crate::Opts;
+    use crate::sys::SandboxImpl;
+    use crate::sys::windows::Sandbox;
 
     #[test]
     fn hello() {
@@ -170,10 +209,15 @@ mod tests {
      * 启动记事本
      */
     #[test]
-    fn notepad() -> Result<()> {
+    fn notepad() {
         let mut opts: Opts = Opts::default();
         opts.command.push("C:/Windows/notepad.exe".to_string());
-        unsafe { Sandbox::with_opts(opts).run() }
+        let status = unsafe {
+            Sandbox::with_opts(opts).run().unwrap()
+        };
+        assert_eq!(status.status, 0);
+        assert_eq!(status.exit_code, 0);
+        assert_eq!(status.signal, 0);
     }
 
     /**
@@ -193,10 +237,14 @@ mod tests {
      * 测试时间限制
      */
     #[test]
-    fn time_limit() -> Result<()> {
+    fn time_limit() {
         let mut opts: Opts = Opts::default();
         opts.command.push("C:/Windows/notepad.exe".to_string());
         opts.time_limit = Some(1000);
-        unsafe { Sandbox::with_opts(opts).run() }
+        let status = unsafe {
+            Sandbox::with_opts(opts).run().unwrap()
+        };
+        assert!(status.time_used >= 1000);
+        assert!(status.time_used < 2000);
     }
 }
